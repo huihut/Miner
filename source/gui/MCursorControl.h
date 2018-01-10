2eb04e56e08ec6c85bb4d3163ee4022961a86ba3
huihut <huihut@outlook.com>
2018-01-11 01:27:52 +0800

    add MCursorControl

---
+++ source/gui/MCursorControl.h
@@ -0,0 +1,25 @@
+//
+// Created by huihut on 2018/1/10.
+//
+
+#ifndef MINER_MCURSORCONTROL_H
+#define MINER_MCURSORCONTROL_H
+
+#include "../core/position2d.h"
+#include "../MUnknown.h"
+#include "../MTypes.h"
+
+namespace miner
+{
+    namespace gui
+    {
+        //! Interface to manipulate the mouse cursor.
+        class MCursorControl : public MUnknow
+        {
+        public:
+
+        };
+    } // end namespace gui
+} // end namespace miner
+
+#endif //MINER_MCURSORCONTROL_H
